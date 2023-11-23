29c0ebc0c35020a22c43504343f18497d8f904a6
EdebaliErman <edebalierman@gmail.com>
2023-11-23 23:24:50 +0300

    Header complated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import TextBar from './Components/TextBar/TextBar';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <img className="bg-desktop" src={process.env.PUBLIC_URL + "assets/bg-desktop.svg"} alt='desktop' />
+      <header>
+        <img className="hunddle" src={process.env.PUBLIC_URL + "assets/logo.svg"} alt='desktop' />
+      </header>
+      <article>
+        <img className="mockups" src={process.env.PUBLIC_URL + "assets/illustration-mockups.svg"} alt='mockups' />
+        <TextBar />
+      </article>
     </div>
   );
+
 }
 
 export default App;
---
+++ src/Components/TextBar/TextBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function TextBar() {
+    return (
+        <div>
+            <h1>Build The Community Your Fans Will Love </h1>
+            <h1>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion. </h1>
+            <button>Register</button>
+        </div>
+    )
+}
+
+export default TextBar
