099620cf9105e1ad6971444fc1a68e479dcc978f
EdebaliErman <edebalierman@gmail.com>
2023-11-24 00:42:04 +0300

    Footer

--- src/Components/TextBar/TextBar.js
+++ src/Components/TextBar/TextBar.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import "./TextBar.css"
 function TextBar() {
     return (
-        <div>
+        <div className='TextBar'>
             <h1>Build The Community Your Fans Will Love </h1>
-            <h1>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion. </h1>
+            <h2>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion. </h2>
             <button>Register</button>
         </div>
     )
